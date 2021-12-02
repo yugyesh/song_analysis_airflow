513329e17d59c4bd7535fbdc5d7e2a46e621e124
yugyesh <uges.kk@gmail.com>
2021-12-02 21:30:32 +0900

    feat: Order the taks in proper sequence

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -61,10 +61,8 @@ stage_songs_to_redshift = StageToRedshiftOperator(
 )
 
 # load_songplays_table = LoadFactOperator(task_id="load_songplays_fact_table", dag=dag)
-songplays_table_columns = (
-    "start_time, user_id, song_id, artist_id, level, session_id, location, user_agent"
-)
-load_songplays_table = LoadDimensionOperator(
+songplays_table_columns = "playid, start_time, userid, level, songid, artistid, sessionid, location, user_agent"
+load_songplays_table = LoadFactOperator(
     task_id="load_songplays_fact_table",
     dag=dag,
     redshift_conn_id="redshift",
@@ -73,31 +71,31 @@ load_songplays_table = LoadDimensionOperator(
     selecting_query=SqlQueries.songplay_table_insert,
 )
 
-user_table_columns = "user_id, first_name, last_name, gender, level"
+user_table_columns = "userid, first_name, last_name, gender, level"
 load_user_dimension_table = LoadDimensionOperator(
     task_id="load_user_dim_table",
     dag=dag,
     redshift_conn_id="redshift",
-    table_name="user",
+    table_name="users",
     column_names=user_table_columns,
     selecting_query=SqlQueries.user_table_insert,
 )
 
-song_table_columns = "song_id, title, artist_id, year, duration"
+song_table_columns = "songid, title, artistid, year, duration"
 load_song_dimension_table = LoadDimensionOperator(
     task_id="load_song_dim_table",
     dag=dag,
     redshift_conn_id="redshift",
-    table_name="song",
+    table_name="songs",
     column_names=song_table_columns,
     selecting_query=SqlQueries.song_table_insert,
 )
 
-artist_table_columns = "artist_id, name, location, latitude, longitude"
+artist_table_columns = "artistid, name, location, latitude, longitude"
 load_artist_dimension_table = LoadDimensionOperator(
     task_id="load_artist_dim_table",
     dag=dag,
-    table_name="artist",
+    table_name="artists",
     redshift_conn_id="redshift",
     column_names=artist_table_columns,
     selecting_query=SqlQueries.artist_table_insert,
@@ -106,13 +104,23 @@ load_artist_dimension_table = LoadDimensionOperator(
 time_table_columns = "start_time, hour, day, week, month, year, weekday"
 load_time_dimension_table = LoadDimensionOperator(
     task_id="load_time_dim_table",
+    table_name="time",
     dag=dag,
     redshift_conn_id="redshift",
     column_names=time_table_columns,
     selecting_query=SqlQueries.time_table_insert,
 )
 
-run_quality_checks = DataQualityOperator(task_id="run_data_quality_checks", dag=dag)
+run_quality_checks = DataQualityOperator(
+    task_id="run_data_quality_checks",
+    dag=dag,
+    redshift_conn_id="redshift",
+    fact_table="songplays",
+    dim_tables=["artists", "users", "time", "songs"],
+    staging_table="staging_songs",
+    check_table="users",
+    check_column="userid",
+)
 
 end_operator = DummyOperator(task_id="stop_execution", dag=dag)
 
@@ -129,3 +137,10 @@ load_songplays_table >> load_artist_dimension_table
 load_songplays_table >> load_song_dimension_table
 load_songplays_table >> load_user_dimension_table
 load_songplays_table >> load_time_dimension_table
+
+load_artist_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
